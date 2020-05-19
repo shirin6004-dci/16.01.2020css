3934ad290917b752a9d25f872c59988181727ee8
shirin6004-dci <shirinvalizadeh6004@gmail>
2020-05-19 09:00:58 +0200

    boostrap

---
+++ boostrap/2-boostrap-jquery/js/script.js
@@ -0,0 +1,92 @@
+//* if pass and user sind true
+
+$(document).ready(function () {
+
+    $('#loginBtn').click(function (e) {   //!1
+        e.preventDefault();
+        let check = true   //!4
+
+        //check user is not empty
+        let username = $('#userNameInput').val();  //!2
+        if (!username.trim()) {
+
+            $('#usernameAlert').removeClass('d-none');
+            check = false  //!4
+        }
+
+        //PASSWORD
+        let password = $('#passwordInput').val();  //!3
+        if (!password.trim()) {
+            $('#passwordAlert').removeClass('d-none');
+            check = false   //!4
+        }
+
+//--------------
+
+
+//if both are true
+if (check) {    //!5 
+    let sendDataObj = {  
+        userName :username ,   
+        password : password
+    }
+    $.ajax({     //!6
+        type: "GET",
+        url: "/usercheck.json",
+        data: JSON.stringify(sendDataObj),
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+            //show  login error modal     
+            if (!response.result) {
+               $('#loginErrorModal').modal('show')     
+            }else{
+                //login success
+                alert('success')
+            }
+      
+        }
+    });
+}
+
+
+//--------------
+//========or =====
+/*
+        resultCheck().then(theResult => {
+            if (theResult == false) {
+                console.log('no');
+                $('#passwordInput').addClass('show');
+            }
+
+        }).catch(error => {
+            console.log(error);
+
+        })
+
+*/
+//===========
+    });
+
+});
+//=========or======
+/*
+function resultCheck() {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: "GET",
+            url: "usercheck.json",
+            data: "data",
+            dataType: "json",
+            success: function (response) {
+                console.log(response);
+                resolve(response.result)
+            },
+            error: function (error) {
+                reject(error)
+            }
+        });
+    })
+} 
+*/
+//==============
---
+++ jQuery/12-pasley-plugin/js/script.js
@@ -0,0 +1,107 @@
+//* if pass and user sind true
+
+$(document).ready(function () {
+
+    $('#loginBtn').click(function (e) {   //!1
+        e.preventDefault();
+        let check = true   //!4
+
+        //check user is not empty
+        let username = $('#userNameInput').val();  //!2
+        if (!username.trim()) {
+
+            $('#usernameAlert').removeClass('d-none');
+            check = false  //!4
+        }
+
+        //PASSWORD
+        let password = $('#passwordInput').val();  //!3
+        if (!password.trim()) {
+            $('#passwordAlert').removeClass('d-none');
+            check = false   //!4
+        }
+
+//--------------
+
+
+//if both are true
+if (check) {    //!5 
+    let sendDataObj = {  
+        userName :username ,   
+        password : password
+    }
+    $.ajax({     //!6
+        type: "GET",
+        url: "/usercheck.json",
+        data: JSON.stringify(sendDataObj),
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+            //show  login error modal     
+            if (!response.result) {
+               $('#loginErrorModal').modal('show')     
+            }else{
+                //login success
+                alert('success')
+            }
+      
+        }
+    });
+}
+
+
+//--------------
+//========or =====
+/*
+        resultCheck().then(theResult => {
+            if (theResult == false) {
+                console.log('no');
+                $('#passwordInput').addClass('show');
+            }
+
+        }).catch(error => {
+            console.log(error);
+
+        })
+
+*/
+//===========
+    });
+
+
+//validate btn
+$('#validateBtn').click(function (e) { 
+    e.preventDefault();
+ $('.card-body input').each((idx, element) =>{
+   let parslayvalidate = $(element).parsley().validate();
+   console.log(parslayvalidate);
+   
+ })
+
+   
+});
+
+
+
+});
+//=========or======
+/*
+function resultCheck() {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            type: "GET",
+            url: "usercheck.json",
+            data: "data",
+            dataType: "json",
+            success: function (response) {
+                console.log(response);
+                resolve(response.result)
+            },
+            error: function (error) {
+                reject(error)
+            }
+        });
+    })
+} 
+*/
+//==============
